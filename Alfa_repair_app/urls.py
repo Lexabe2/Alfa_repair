2c8069ef1325ebd7ca9b26fcb920ea54ab983127
aleksej <natash79@inbox.ru>
2025-08-07 08:44:56 +0300

    Добавил трек номера

--- Alfa_repair_app/urls.py
+++ Alfa_repair_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import index, add_bank_req, login_aut, login_views, out, acceptance, acceptance_terminal, distribution, \
-    add_data_all, export_serials_to_excel
-
+    add_data_all
 urlpatterns = [
     path('login/', login_views, name='login'),
     path('out/', out, name='out'),
@@ -12,5 +11,4 @@ urlpatterns = [
     path('acceptance_terminal/', acceptance_terminal, name='acceptance_terminal'),
     path('distribution/', distribution, name='distribution'),
     path('add_data_all/', add_data_all, name='add_data_all'),
-    path('export-serials/', export_serials_to_excel, name='export_serials'),
 ]
